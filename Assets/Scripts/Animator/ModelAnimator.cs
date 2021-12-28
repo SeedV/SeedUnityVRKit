7161c62ea00adeb47f6d601e0d922acaad650af6
Henry Ou <henryouly@gmail.com>
2021-12-28 07:30:24 -0800

    Add ModelAnimator.

---
+++ Assets/Scripts/Animator/ModelAnimator.cs
@@ -0,0 +1,35 @@
+// Copyright 2021-2022 The SeedV Lab.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections;
+using System.Collections.Generic;
+
+using Logger = Mediapipe.Logger;  // Disambiguation with UnityEngine.Logger
+using UnityEngine;
+
+public class ModelAnimator : MonoBehaviour {
+  private const string _TAG = nameof(ModelAnimator);
+  private Animator _anim;
+  private Transform _hip;
+
+  void Start() {
+    _anim = GetComponent<Animator>();
+    _hip = _anim.GetBoneTransform(HumanBodyBones.Hips);
+  }
+
+  void Update() {
+    // Test code
+    _hip.transform.rotation = Quaternion.Euler(0, 45, -90);
+  }
+}
