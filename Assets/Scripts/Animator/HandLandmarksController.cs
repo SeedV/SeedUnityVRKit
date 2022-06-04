433e1786be1b1a75345055a14ff262f65f18a25e
Henry Ou <henryouly@gmail.com>
2022-06-04 15:57:59 -0700

    Track index finger.

--- Assets/Scripts/Animator/HandLandmarksController.cs
+++ Assets/Scripts/Animator/HandLandmarksController.cs
@@ -15,6 +15,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Color = UnityEngine.Color;
+
+using Mediapipe;
+using Mediapipe.Unity;
 
 namespace SeedUnityVRKit {
   public enum HandType { LeftHand, RightHand }
@@ -24,19 +28,45 @@ namespace SeedUnityVRKit {
     [Tooltip("Left hand or right hand.")]
     public HandType handType;
 
+    [Tooltip("Scale factor to properly convert mediapipe landmarks to world space.")]
+    public Vector3 scale = new Vector3(0.2f, 0.2f, 0.2f);
+
     // The hand root to keep track of the position and rotation.
     private Transform _target;
 
+    private GameObject _indexTip;
+
     void Start() {
       var bone =
-          (handType == HandType.LeftHand) ? HumanBodyBones.LeftHand : HumanBodyBones.RightHand;
+          (handType == HandType.LeftHand) ? HumanBodyBones.RightHand : HumanBodyBones.LeftHand;
       _target = anim.GetBoneTransform(bone);
+
+      _indexTip = new GameObject("IndexTip");
+      _indexTip.transform.parent = transform;
+    }
+
+    void Update() {
+      transform.position = _target.transform.position;
     }
 
     void OnDrawGizmos() {
+      Gizmos.color = Color.red;
       if (_target != null) {
-        Gizmos.color = Color.red;
         Gizmos.DrawSphere(_target.position, 0.005f);
+      }
+      if (_indexTip != null) {
+        Gizmos.DrawSphere(_indexTip.transform.position, 0.005f);
+      }
+    }
+
+    public void OnHandLandmarksOutput(NormalizedLandmarkList landmarkList) {
+      if (landmarkList != null) {
+        NormalizedLandmark landmark8 = landmarkList.Landmark[8];
+        NormalizedLandmark landmark0 = landmarkList.Landmark[0];
+        Vector3 tip = Vector3.Scale(new Vector3(landmark8.X, landmark8.Y, landmark8.Z) -
+                                        new Vector3(landmark0.X, landmark0.Y, landmark0.Z),
+                                    scale);
+        _indexTip.transform.localPosition = tip;
       }
     }
   }
