63e8f2d3bf9a3e7144d1dc8c0272b7aebde9ff72
Henry Ou <henryouly@gmail.com>
2022-06-13 09:09:07 -0700

    Add some documentation to Kalman Filter.

--- Assets/Scripts/Animator/KalmanFilter.cs
+++ Assets/Scripts/Animator/KalmanFilter.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 namespace SeedUnityVRKit {
   // <summary>
   // Kalman filter implementation for <c>Vector3</c>.
+  //
+  // A good interactive tutorial for beginners about Kalman filter is
+  // https://simondlevy.academic.wlu.edu/kalman-tutorial/.
+  //
+  // This is a scalar version, only models one variable (position).
   // </summary>
   public class KalmanFilter {
+    // Process noise models noise to an "ideal" estimated state transition. The larger the
+    // value, the less accurate the new estimation is based on law of physics.
     private readonly float _q;
+    // Measure noise models noise in the measurement process. The larger the value is, the
+    // less accurate the measure is.
     private readonly float _r;
+    // Prediction error is used to update the Kalman gain. It is recursively updated from its
+    // last value with Kalman gain.
+    // p_k = (1 – g_k) * p_(k−1)
     private float _p = 0.1f;
+    // Prediction is recursively updated with
+    // x_k = x_(k-1) + g_k * (z_k - x_(k-1))
+    // It presents the most likely position of the object at given observation.
     private Vector3 _x;
+    // The Kalman gain is used to trade-off between estimated state from last time and current
+    // measurement. Kalman gain is 0 means the estimated state is correct. Kalman gain is 1 means
+    // the measured value (observation) is correct. It can be updated using prediction errors from
+    // last state.
+    // g_k = p_(k-1) / (p_(k-1) + r)
     private float _k;
 
     public KalmanFilter(float q, float r) {
