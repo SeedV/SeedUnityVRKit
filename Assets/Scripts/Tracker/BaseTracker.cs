9d2560de09fcdb7509b81f903dde65d63d9bcef5
sunzhifeng <sunzhifeng@chuangxin.com>
2022-07-15 11:51:12 +0800

    Fixed the callback null bug

--- Assets/Scripts/Tracker/BaseTracker.cs
+++ Assets/Scripts/Tracker/BaseTracker.cs
@@ -96,7 +96,7 @@ namespace SeedUnityVRKit {
         systemStats?.IncrementFrameReadTexture();
         _graphRunner.AddTextureFrameToInputStream(textureFrame);
         systemStats?.IncrementFrameAddToInputStream();
-        yield return null;
+        yield return new WaitForEndOfFrame();
       }
     }
 
